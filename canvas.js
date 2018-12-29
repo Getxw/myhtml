bd20d0f3e2325b5fd7f6e63a863a227151302842
Getxw <41560686+Getxw@users.noreply.github.com>
2018-12-29 16:41:56 +0800

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -4,22 +4,22 @@ function draw(){
     // draw();
     ctx.beginPath();
 	ctx.moveTo(300,150);
-    ctx.strokeStyle="#f00";
 	ctx.quadraticCurveTo(150,50,120,200);
 	ctx.quadraticCurveTo(100,350,300,500);
 	ctx.quadraticCurveTo(500,350,480,200);
 	ctx.quadraticCurveTo(450,50,300,150);
     ctx.stroke();
-	ctx.fillStyle="#f00";
+	ctx.fillStyle="black";
 	// ctx.fill();
 	ctx.shadowColor = "gray"; 
-    ctx.shadowOffsetX = 10; 
-    ctx.shadowOffsetY = 5; 
+    ctx.shadowOffsetX = 20; 
+    ctx.shadowOffsetY = -10; 
     ctx.shadowBlur = 5; 
     ctx.fill();
+    ctx.fillStyle="black";
     ctx.font="24px 宋体";
     ctx.fillText("Thanks for your accompany all of my days.",50,540);
-    ctx.fillText("---他是个宝宝",400,590);
+    ctx.fillText("---坏蛋",400,590);
 
 }
 draw();
