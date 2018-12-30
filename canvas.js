227444f8a768b3f6a1d29da98d04253157fb9e5f
Getxw <41560686+Getxw@users.noreply.github.com>
2018-12-30 16:45:08 +0800

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -2,24 +2,21 @@ var d=document.getElementById("Mycanvas");
 var ctx=d.getContext("2d");
 function draw(){
     // draw();
-    ctx.beginPath();
+    ctx.font="24px 宋体";
+    ctx.fillText("Thanks for your accompany all of my days.",50,540);
+    ctx.fillText("---坏蛋",400,590);
 	ctx.moveTo(300,150);
 	ctx.quadraticCurveTo(150,50,120,200);
 	ctx.quadraticCurveTo(100,350,300,500);
 	ctx.quadraticCurveTo(500,350,480,200);
 	ctx.quadraticCurveTo(450,50,300,150);
-    ctx.stroke();
+    // ctx.stroke();
 	ctx.fillStyle="black";
-	// ctx.fill();
-	ctx.shadowColor = "gray"; 
+	ctx.shadowColor = "#232020"; 
     ctx.shadowOffsetX = 20; 
     ctx.shadowOffsetY = -10; 
     ctx.shadowBlur = 5; 
     ctx.fill();
-    ctx.fillStyle="black";
-    ctx.font="24px 宋体";
-    ctx.fillText("Thanks for your accompany all of my days.",50,540);
-    ctx.fillText("---坏蛋",400,590);
 
 }
 draw();
