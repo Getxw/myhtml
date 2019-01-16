023c6cd2a56047c33e1f932e63d2890bdc57da9f
Getxw <41560686+Getxw@users.noreply.github.com>
2019-01-16 13:29:17 +0800

    Update hera.js

--- hera.js
+++ hera.js
@@ -1,12 +1,13 @@
 try{
-            if (/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
-                // window.location.href="mindex.html";
-                alert("手机滚！");
-            }else{
-                // window.location.href="cindex.html";
-                alert("欢迎爸爸!");
-                }
-            }catch(e){}
+      if (/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
+          // window.location.href="mindex.html";
+          alert("手机滚！");
+	  window.close();
+       }else{
+          // window.location.href="cindex.html";
+          alert("欢迎爸爸!");
+       }
+   }catch(e){}
 var i=0;
 var arr=new Array();
 arr[0]="images/0.jpg";
